2b0afe86316981243510eb1a21fb41f026c90c3c
Andre Galioni <deh.galioni@gmail.com>
2026-02-18 15:55:03 +0000

    testing vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // This MUST match your repository name on GitHub
-  // If your repo is https://github.com/galioni/gym, the base is '/gym/'
   base: '', 
   build: {
     outDir: 'dist',
